059ee67e00f4b6319f772d6354b265d5494540bd
iceoffire <ugandini@gmail.com>
2018-05-15 11:22:43 -0300

    new files

--- ninja plat/ninja.py
+++ ninja plat/ninja.py
@@ -15,17 +15,64 @@ def load():
 	return True
 
 def load_vars():
-	global screen_size, screen, game_object, folder, cam
+	global screen_size, screen, game_object, folder, cam, contador
 	folder = os.path.dirname(os.path.realpath(__file__))
-	screen_size = (300, 300)
+	screen_size = (600, 600)
 	screen = pygame.display.set_mode(screen_size)
-	game_object = {
-		"player" : [],
-		"bg"	 : [],
-		"enemy"  : [],
-		"HUD"    : []
+	contador = 0
+	game_object = {'bg' : [Sprite(0, 0, "/assets/img/tiles/bg.png")],
+		'HUD'   : [],
+		'tiles' : [],
+		'enemy' : [],
+		'player': [],
+		
 	}
+	print(game_object)
 	cam = Camera(-screen_size[0]/2, -screen_size[1]/2)
+
+def update():
+	k = pygame.key.get_pressed()
+	if k[K_d]:
+		cam.x += 5
+	elif k[K_a]:
+		cam.x -= 5
+	if k[K_s]:
+		cam.y += 5
+	elif k[K_w]:
+		cam.y -= 5
+	pass
+
+def draw():
+	screen.fill((0, 0, 0))
+	camera()
+	pygame.display.flip()
+	fps(60)
+	pass
+
+def fps(frames):
+	pygame.time.Clock().tick(frames)
+
+def camera():
+	global contador
+	x = cam.x
+
+	y = cam.y
+	image_teste = game_object['tiles'][0].img
+	for name in game_object:
+		if contador < 5:
+			print(name)
+			contador +=1
+		for gO in game_object[name]:
+			temp_img = gO.img #pygame.transform.scale(gO.img, (160,160))
+			screen.blit(temp_img, (gO.x+x, gO.y+y))
+#			screen.blit(image_teste, (gO.x+x, gO.y+y))
+
+def check_exit():
+	k = pygame.key.get_pressed()
+	for e in pygame.event.get():
+		if e.type==QUIT or k[K_ESCAPE]:
+			return False
+	return True
 
 def load_map(map):
 	path = "/assets/img/map/map" + str(map) + ".png"
@@ -35,38 +82,17 @@ def load_map(map):
 	for linha in range(height):
 		for coluna in range(width):
 			color = img.get_at((linha, coluna))
-			load_object(str(color), linha*32, coluna*32)
+			load_object(str(color), linha*128, coluna*128)
 	pass
 
 def load_object(color, x, y):
 	objects = {
-		'(0, 0, 0, 255)'       : ['bg','/assets/img/tiles/floor.png'], #black
-		'(255, 255, 255, 255)' : ['bg','/assets/img/tiles/bg.png'], #white
+		'(0, 0, 0, 255)'       : ['tiles','/assets/img/tiles/floor.png'], #black
 	}
 	if color in objects:
 		game_object[objects[color][0]].append(Sprite(x, y, objects[color][1]))
 
-def update():
-	pass
-
-def draw():
-	screen.fill((0, 0, 0))
-	camera()
-	pygame.display.flip()
-	pass
-
-def camera():
-	for name in game_object:
-		for gO in game_object[name]:
-			screen.blit(gO.img, (gO.x, gO.y))
-
-def check_exit():
-	k = pygame.key.get_pressed()
-	for e in pygame.event.get():
-		if e.type==QUIT or k[K_ESCAPE]:
-			return False
-	return True
-
+#Classes
 class Sprite():
 	def __init__(self, x, y, path, scale=1, width=None, height=None):
 		self.x = x
