6a4d491fd6b05c8e30fa1c85c5755d694aa12538
Ulisses Gandini <ugandini@gmail.com>
2018-05-28 05:24:04 -0300

    pitfall

--- pitfall/main.py
+++ pitfall/main.py
@@ -11,7 +11,7 @@ def main():
     pygame.quit()
 
 def load():
-    scale = 1
+    scale = 2
     screen_size = (320*scale, 210*scale)
     #screen_size = (640, 420)
     screen = pygame.display.set_mode(screen_size)
@@ -39,34 +39,16 @@ def load():
         'level'         : 1,
         'seconds_left'  : 1200,
         'hp'            : 2000,
-        'lifes'         : 3,
+        'life'          : 3,
         'exit'          : False,
         'init'          : time(),
         'layer'         : ['bg', 'ladder', 'floor', 'wall', 'water', 'hole', 'water_croco', 'wood', 'vine', 'HUD', 'player'],
         'scale'         : scale
     }
 
-    sprite = {
-        'idle'      : [1, 0],
-        'running'   : [5, 0.3],
-        'vine'      : [1, 0],
-        'jumping'   : [1, 0],
-        'climbing'  : [2, 0]
-    }
-
     #loading first map
-    game_object['player'].append(Sprite(30, 110, var['folder']+'/assets/img/player/idle0.png', (30, 380, 16, 22), \
-        (sprite, var['folder'] + '/assets/img/player', 'idle')))
 
-    game_object['bg'].append(Sprite(16, 13, var['folder']+'/assets/img/bg/bg.png'))
-
-    game_object['floor'].append(Sprite(16, 124, var['folder']+'/assets/img/floor/floor.png'))
-
-    game_object['wall'].append(Sprite(280, 155, var['folder']+'/assets/img/wall/wall.png'))
-    
-    game_object['ladder'].append(Sprite(156, 140, var['folder']+'/assets/img/ladder/ladder.png'))
-
-    game_object['wood'].append(Sprite(260, 123, var['folder']+'/assets/img/wood/wood0.png'))
+    game_object = load_level(game_object, var, 1)
 
     return True, {
         'screen_size'   : screen_size,
@@ -75,8 +57,79 @@ def load():
         'var'           : var,
     }
 
+
+def load_level(game_object, var, what_level):
+    if what_level==1:
+        sprite = {
+            'idle'      : [1, 0],
+            'running'   : [5, 0.07],
+            'vine'      : [1, 0],
+            'jumping'   : [1, 0],
+            'climbing'  : [2, 0]
+        }
+        game_object['player'].append(Sprite(30, 110, var['folder']+'/assets/img/player/idle0.png', (30, 380, 16, 22), \
+        (sprite, var['folder'] + '/assets/img/player', 'idle')))
+
+        game_object['bg'].append(Sprite(16, 13, var['folder']+'/assets/img/bg/bg.png'))
+
+        game_object['floor'].append(Sprite(16, 124, var['folder']+'/assets/img/floor/floor.png'))
+
+        game_object['wall'].append(Sprite(280, 155, var['folder']+'/assets/img/wall/wall.png'))
+        
+        game_object['ladder'].append(Sprite(156, 140, var['folder']+'/assets/img/ladder/ladder.png'))
+
+        game_object['wood'].append(Sprite(260, 123, var['folder']+'/assets/img/wood/wood0.png'))
+    return game_object
+
+
 def update(settings):
     settings['var']['seconds_left'] = 1201-(time()-settings['var']['init'])
+    settings['game_object']['player'][0].animation.update()
+    player = settings['game_object']['player'][0]
+    k = pygame.key.get_pressed()
+    if k[K_d] and player.grounded:
+        settings['game_object']['player'][0].x +=1
+        settings['game_object']['player'][0].side = False
+        if settings['game_object']['player'][0].animation.tile == 'idle':
+            settings['game_object']['player'][0].animation.change('running')
+    elif k[K_a] and player.grounded:
+        settings['game_object']['player'][0].x -=1
+        settings['game_object']['player'][0].side = True
+        if settings['game_object']['player'][0].animation.tile == 'idle':
+            settings['game_object']['player'][0].animation.change('running')
+    else:
+        if settings['game_object']['player'][0].animation.tile == 'running':
+            settings['game_object']['player'][0].animation.change('idle')
+    if player.grounded and k[K_SPACE] and (k[K_d] or k[K_a]):
+        if k[K_d]:
+            settings['game_object']['player'][0].x_speed = 1.1
+        else:
+            settings['game_object']['player'][0].x_speed = -1.1
+        settings['game_object']['player'][0].y_speed = -13
+        settings['game_object']['player'][0].animation.change('jumping')
+        settings['game_object']['player'][0].grounded = False
+    elif player.grounded and k[K_SPACE]:
+        settings['game_object']['player'][0].y_speed = -13
+        settings['game_object']['player'][0].x_speed = 0
+        settings['game_object']['player'][0].animation.change('jumping')
+        settings['game_object']['player'][0].grounded = False
+    if settings['game_object']['player'][0].y_speed >0:
+        settings['game_object']['player'][0].y += 1
+        settings['game_object']['player'][0].x += settings['game_object']['player'][0].x_speed
+    elif settings['game_object']['player'][0].y_speed<0:
+        settings['game_object']['player'][0].y -= 1
+        settings['game_object']['player'][0].x += settings['game_object']['player'][0].x_speed
+        settings['game_object']['player'][0].y_speed += 1
+    else:
+        if settings['game_object']['player'][0].y !=110:
+            settings['game_object']['player'][0].y -= 1
+            settings['game_object']['player'][0].y_speed += 1
+    if settings['game_object']['player'][0].y >110:
+        settings['game_object']['player'][0].y = 110
+        settings['game_object']['player'][0].y_speed = 0
+        settings['game_object']['player'][0].animation.change('idle')
+        settings['game_object']['player'][0].grounded = True
+
     return settings
 
 def draw(settings):
@@ -95,13 +148,21 @@ def draw(settings):
         for gO in game_object[name]:
             if gO.__class__==Sprite:
                 temp_img = pygame.transform.scale(gO.img, (gO.img.get_width()*scale, gO.img.get_height()*scale))
+                temp_img = pygame.transform.flip(temp_img, gO.side, False)
                 screen.blit(temp_img, (gO.x*scale, gO.y*scale))
-    font = pygame.font.SysFont("PressStart2P", 15)
+    font = pygame.font.SysFont("PressStart2P", 15*scale)
     
-    screen.blit(font.render(hp, True, (230, 230, 230)), (75, 17))
-    screen.blit(font.render(time, True, (230, 230, 230)), (60, 33))
+    for life in range(settings['var']['life']):
+        pygame.draw.rect(screen, (230, 230, 230), ((36+6*life)*scale, 33*scale, 3*scale, 12*scale))
+
+    screen.blit(font.render(hp, True, (230, 230, 230)), (75*scale, 17*scale))
+    screen.blit(font.render(time, True, (230, 230, 230)), (60*scale, 33*scale))
     pygame.display.flip()
+    fps(60)
     pass
+
+def fps(frames):
+    pygame.time.Clock().tick(frames)
 
 def check_exit(settings):
     if settings['var']['exit']: return False
@@ -118,6 +179,10 @@ class Sprite():
         self.y              = y
         self.path           = path
         self.img            = pygame.image.load(path)
+        self.grounded       = True
+        self.y_speed        = 0
+        self.x_speed        = 0
+        self.side           = False
         if collider != None:
             #                         (x          , y          , width      , height     , obj)
             self.collider   = Collider2D(collider[0], collider[1], collider[2], collider[3], self)
