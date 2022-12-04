ba227844a14b2f7b6e669ee4c48cd2504d250719
iceoffire <ugandini@gmail.com>
2022-12-04 19:25:21 -0300

    updating Surface POC project

--- Projects/POC Surface/main.py
+++ Projects/POC Surface/main.py
@@ -2,6 +2,7 @@ import pygame
 from pygame.locals import *
 from os.path import dirname, realpath
 
+
 def main():
     running, settings = load()
     while running:
@@ -10,28 +11,26 @@ def main():
         running = check_exit(**settings)
     pygame.quit()
 
+
 def load():
     screen_size = (640, 500)
     screen = pygame.display.set_mode(screen_size)
     exit_request = False
-    var = {
-        'folder' : dirname(realpath(__file__))
-    }
-
     my_surface = load_level(1)
     my_surface = pygame.transform.scale(my_surface, screen_size)
     return True, {
-        'screen_size'   : screen_size,
-        'screen'        : screen,
-        'my_surface'    : my_surface,
-        'exit_request'  : exit_request,
-        'x'             : 0,
+        'screen_size': screen_size,
+        'screen': screen,
+        'my_surface': my_surface,
+        'exit_request': exit_request
     }
 
+
 def load_level(level):
-    image = pygame.image.load(dirname(realpath(__file__))+'/img/map/map'+str(level)+'.png')
+    image = pygame.image.load(
+        dirname(realpath(__file__))+'/img/map/map'+str(level)+'.png')
     width = image.get_width()
-    height =image.get_height()
+    height = image.get_height()
     my_surface = pygame.Surface((width*128, height*128))
     for row in range(height):
         for column in range(width):
@@ -40,12 +39,14 @@ def load_level(level):
             color = str(image.get_at((column, row)))
             obj_img = get_object(color)
             if obj_img != False:
-                my_surface.blit(pygame.image.load(dirname(realpath(__file__))+obj_img),(x, y))
+                my_surface.blit(pygame.image.load(
+                    dirname(realpath(__file__))+obj_img), (x, y))
     return my_surface
 
-def get_object(color):# -> Surface
+
+def get_object(color):  # -> Converts Color to Surface Image
     obj_mem = {
-        '(0, 0, 0, 255)' : '/img/tile/FMF.png'
+        '(0, 0, 0, 255)': '/img/tile/FMF.png'
     }
     if color in obj_mem:
         return obj_mem[color]
@@ -54,24 +55,25 @@ def get_object(color):# -> Surface
 
 
 def update(settings):
-#    settings['x'] -=1
     for e in pygame.event.get():
         if e.type == QUIT or (e.type == KEYDOWN and e.key == K_ESCAPE):
             settings['exit_request'] = True
     return settings
 
+
 def draw(screen_size, screen, my_surface, x, **kwargs):
     screen.fill((0, 0, 0))
-    #temp_img = pygame.transform.scale(my_surface, screen_size)
-    screen.blit(my_surface, (x, 0))
+    screen.blit(my_surface, (0, 0))
     pygame.display.flip()
-#    fps(60)
     pass
+
 
 def fps(frame):
     pygame.time.Clock().tick(frame)
 
+
 def check_exit(exit_request, **kwargs):
     return not exit_request
+
 
 main()
